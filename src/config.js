20c7885582e7ef05967c7d3b8461d1a91bc1885e
Siena <siena.cizdziel@yale.edu>
2020-10-23 00:37:00 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-siena"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://3g1xdtan90.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_ahKqzMh2C",
+      APP_CLIENT_ID: "7kso866ta2skhm8chhuhv1pdhc",
+      IDENTITY_POOL_ID: "us-east-2:3d9244fe-6bfb-40cb-b525-436d9ec59c31"
+    }
+  };
